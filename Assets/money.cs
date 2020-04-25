cfe5933c795767352e43a13733929e67f6615614
Henrik Trehjørningen <Henriktre@gmail.com>
2020-04-25 19:41:24 +0200

    merged money script

---
+++ Assets/money.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class money : MonoBehaviour
+{
+    public TextMeshProUGUI moneyTxt;
+    public int cash = 1000;
+    public int wage = 1;
+    public float increment = 1.0f;
+    float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = increment;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter > 0.0f)
+        {
+            counter -= Time.deltaTime;
+
+        }
+        else
+        {
+            cash += wage;
+            moneyTxt.text = cash.ToString() + "$";
+            counter = increment;
+        }
+
+    }
+
+}
