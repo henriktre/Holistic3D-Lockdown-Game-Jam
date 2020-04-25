8158b3ef5eea337f4525a4020c8a0e0999e6fd0f
unknown <stianbso@stud.ntnu.no>
2020-04-25 19:36:34 +0200

    scripts for enemy, enemy controller and gamecontroller

---
+++ Assets/Scripts/EnemyControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerScript : MonoBehaviour
+{
+
+    //GameObject enemyScript;
+    //EnemyAIScript enemyS;
+
+    public EnemyAIScript enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  enemyScript = GameObject.Find("Main Camera");
+        //  enemyS = enemyScript.GetComponent<EnemyAIScript>();
+
+        enemy = new EnemyAIScript();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    float coverupStat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        coverupStat = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
